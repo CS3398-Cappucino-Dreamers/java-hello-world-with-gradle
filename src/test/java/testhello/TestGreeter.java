c3646251e8d97d2c7b2105591d5a1c0c9eef1427
mmp67 <31483557+mmp67@users.noreply.github.com>
2017-10-25 18:21:17 -0500

    Michelle fail?
    Michelle fail....?

--- src/test/java/testhello/TestGreeter.java
+++ src/test/java/testhello/TestGreeter.java
@@ -32,6 +32,6 @@ public class TestGreeter {
    {
       g.setName("World War");
       assertEquals(g.getName(),"World War");
-      assertEquals(g.sayHello(),"Hello I'm sleepy");
+      assertEquals(g.sayHello(),"Why Hello There :)");
    }
 }
