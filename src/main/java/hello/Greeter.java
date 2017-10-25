78ab8a9277e76995dd39cf66b64ae564d5c70f07
Ben Hunt <wbh26@txstate.edu>
2017-10-25 18:17:05 -0500

    Added Test crazyTest

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,9 +1,23 @@
 package hello;
 
 public class Greeter {
-
+	
+	
   private String name = "";
+  private ArrayList<Object> chrisGreeters;
 
+  public Greeter(){
+	 initChris();
+  }
+  
+  public initChris(){
+	  chrisGreeters = new ArrayList<Object>();
+	  for(int i = 0; i<10; i++){
+		  chrisGreeters.add(new Greeter()
+	  }
+  }
+  
+  
   public String getName() 
   {
     return name;
@@ -24,5 +38,19 @@ public class Greeter {
     {
        return "Hello " + name + "!";
     }
+  }
+  
+  @Override
+  public String toString(){
+	  System.out.println("Chris says no ya ya no no ya");
+  }
+  
+  public Object chrisGreetsEverybody(){
+	  if(chrisGreeters != null){
+		  for(Greeter chrisGreeter: chrisGreeters){
+			  chrisGreeter.toString();
+		  }
+	  }
+	  return chrisGreeter;
   }
 }
